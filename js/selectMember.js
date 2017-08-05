d1db9de699df8fed06c2503f6de25b9db1c2bab5
allenfeng <allenfeng@mintcode.com>
2017-08-06 01:07:10 +0800

    css

--- js/selectMember.js
+++ js/selectMember.js
@@ -2,10 +2,10 @@
  * Created by AllenFeng on 2017/5/10.
  */
 (function ($) {
-    $.fn.selectMember = function (teamArray,members) {
+    $.fn.selectMember = function (teamArray, members) {
         var self = this,
-            members =members instanceof Array ?members:[];
-            selectTmpl = '{{#members}}<div class="tag"><span>{{value}}</span><i class="handle icon-glyph-167" data-code="{{code}}"></i></div>{{/members}}',
+            members = members instanceof Array ? members : [];
+        selectTmpl = '{{#members}}<div class="tag"><span>{{value}}</span><i class="handle icon-glyph-167" data-code="{{code}}"></i></div>{{/members}}',
             teamTmpl = '<ul class="team-container">{{#team}}<li class="team"><i class="toggle icon-glyph-166"></i><input class="team-check" type="checkbox" data-code="{{teamId}}" data-value="{{team}}"/><span>{{team}}</span>\
            <ul class="member-container">{{#members}}\
         <li class="member"><input class="member-check" data-code="{{name}}" data-value="{{trueName}}"type="checkbox"/><span>{{trueName}}</span>\
@@ -19,25 +19,25 @@
         self.append(teamTemplate({team: teamArray}));
 
         function _checkRender() {
-            $('.select-input').html(selectTemplate({members:members}));
-            $('.team-container input').prop('checked',false);
+            $('.select-input').html(selectTemplate({members: members}));
+            $('.team-container input').prop('checked', false);
             members.map(function (item) {
-                $('.team-container input[data-code="' + item.code + '"]').prop('checked',true);
+                $('.team-container input[data-code="' + item.code + '"]').prop('checked', true);
             });
-            $.each($('.team'),function (index,item) {
-                var checked=true;
-                var children=$(item).find('.member-check');
-                $.each(children,function (index,child) {
-                    if(!$(child).prop('checked')){
-                        checked=false
+            $.each($('.team'), function (index, item) {
+                var checked = true;
+                var children = $(item).find('.member-check');
+                $.each(children, function (index, child) {
+                    if (!$(child).prop('checked')) {
+                        checked = false
                     }
                 })
-                $(item).find('.team-check').prop('checked',checked);
+                $(item).find('.team-check').prop('checked', checked);
             })
         }
 
         //default values render
-        members&&members.length>0&&_checkRender();
+        members && members.length > 0 && _checkRender();
 
         //delete member
         self.on('click', '.handle', function (e) {
@@ -66,10 +66,10 @@
             var children = $check.parent().find('.member-check');
             if ($check.prop('checked')) {
                 $.each(children, function (index, item) {
-                    var i=_.findIndex(members,function (item) {
-                        return item.code==$(item).data('code')
+                    var i = _.findIndex(members, function (item) {
+                        return item.code == $(item).data('code')
                     })
-                    i<0&&members.push({code: $(item).data('code'), value: $(item).data('value')})
+                    i < 0 && members.push({code: $(item).data('code'), value: $(item).data('value')})
                 })
             } else {
                 $.each(children, function (index, item) {
@@ -86,10 +86,10 @@
             e.stopPropagation();
             var $check = $(this);
             if ($check.prop('checked')) {
-                var i=_.findIndex(members,function (item) {
-                    return item.code==$check.data('code')
+                var i = _.findIndex(members, function (item) {
+                    return item.code == $check.data('code')
                 })
-                i<0&&members.push({code: $check.data('code'), value: $check.data('value')});
+                i < 0 && members.push({code: $check.data('code'), value: $check.data('value')});
             } else {
                 _.remove(members, function (item) {
                     return item.code == $check.data('code');
